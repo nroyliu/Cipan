8e1a9ccaaab949f696dd4e084fd67cec683830ae
Selectnn <Nroycc@gmail.com>
2019-08-06 10:37:39 +0800

    初始化

---
+++ application/member/controller/Account.php
@@ -0,0 +1,11 @@
+<?php
+namespace app\member\controller;
+
+use think\Controller;
+
+class account extends Controller
+{
+    public function index(){
+        return $this->fetch("/member/index");
+    }
+}
